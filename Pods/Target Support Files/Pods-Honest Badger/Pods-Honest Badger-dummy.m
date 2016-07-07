69e3cf610d6d7b2ed3b0adc2dc285028c2e2942b
Steve Cox <coxsteve@gmail.com>
2016-07-07 12:37:27 -0600

    Added Firebase to Project
    Firebase:
    Analytics
    Auth
    Database

---
+++ Pods/Target Support Files/Pods-Honest Badger/Pods-Honest Badger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Honest_Badger : NSObject
+@end
+@implementation PodsDummy_Pods_Honest_Badger
+@end
---
+++ Pods/Target Support Files/Pods-Honest Badger/Pods-Honest Badger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Honest_BadgerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Honest_BadgerVersionString[];
+
