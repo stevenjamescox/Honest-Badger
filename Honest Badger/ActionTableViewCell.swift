1c68018bed38e1bafcf9fbdd545c8aad5495da50
Steve Cox <coxsteve@gmail.com>
2016-07-18 14:27:54 -0600

    more files, lots of subclassing

---
+++ Honest Badger/ActionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActionTableViewCell.swift
+//  Honest Badger
+//
+//  Created by Steve Cox on 7/18/16.
+//  Copyright © 2016 stevecoxio. All rights reserved.
+//
+
+import UIKit
+
+class ActionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Honest Badger/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Honest Badger
+//
+//  Created by Steve Cox on 7/18/16.
+//  Copyright © 2016 stevecoxio. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
