7fc93eaeadeba478acfd6db006873a13489efd7d
Steve Cox <coxsteve@gmail.com>
2016-07-27 12:01:37 -0600

    adjustments made, but clock shows 17 extra hours??

--- Honest Badger/QuestionTableViewCell.swift
+++ Honest Badger/QuestionTableViewCell.swift
@@ -15,31 +15,23 @@ class QuestionTableViewCell: UITableViewCell, UITableViewDelegate {
     
     @IBOutlet weak var submitResponseButton: UIButton!
     @IBOutlet weak var viewResponsesButton: UIButton!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(timerFired(_:)), userInfo: nil, repeats: true)
     }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
+    
+    var question: Question?
     
     var timer: NSTimer?
-    
-    var questionPerm: Question?
-    
     var formatter = NSDateFormatter()
     
     func timerFired(timer: NSTimer?){
         
-        guard let question = self.questionPerm else
+        guard let question = self.question else
         
         { return }
-        
-        formatter.dateFormat = "H:mm:ss"
 
-        
         let dateComparisonResult: NSComparisonResult = NSDate().compare(question.timeLimit)
         if dateComparisonResult == NSComparisonResult.OrderedAscending
             
@@ -47,25 +39,17 @@ class QuestionTableViewCell: UITableViewCell, UITableViewDelegate {
             submitResponseButton.setTitle("Submit Response", forState: .Normal)
             submitResponseButton.backgroundColor = UIColor(red: 133/255, green: 178/255, blue: 131/255, alpha: 1)
          
+            formatter.dateFormat = "H:mm:ss"
             let interval = question.timeLimit.timeIntervalSinceDate(NSDate())
             let date = NSDate(timeIntervalSince1970: interval)
             let formattedDateString = formatter.stringFromDate(date)
-    
-            let printLimit = question.timeLimit.timeIntervalSince1970
-            //print(printLimit)
-            
-            let printNow = NSDate().timeIntervalSince1970
-            //print(printNow)
-            
-            let difference: Double = printLimit - printNow
-            print(difference)
-          
-            viewResponsesButton.setTitle("\(formattedDateString) left", forState: .Normal)
+
+            viewResponsesButton.setTitle("\(formattedDateString) left\n to respond", forState: .Normal)
             viewResponsesButton.backgroundColor = .whiteColor()
             
         } else {
             
-            submitResponseButton.setTitle(" \(question.responses.count) responses", forState: .Normal)
+            submitResponseButton.setTitle(" \(question.responses.count) responses\n    received", forState: .Normal)
             submitResponseButton.backgroundColor = .whiteColor()
             viewResponsesButton.setTitle("View Responses", forState: .Normal)
             viewResponsesButton.backgroundColor = UIColor(red: 249/255, green: 81/255, blue: 197/255, alpha: 1)
@@ -73,15 +57,8 @@ class QuestionTableViewCell: UITableViewCell, UITableViewDelegate {
     }
 
     func loadQuestionInfo(question: Question) {
-        self.questionPerm = question
+        self.question = question
         self.timerFired(nil)
         questionLabel.text = "  \(question.questionText)"
     }
-    
 }
-
-
-
-
-
-
