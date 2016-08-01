14d812545cf3d7e3907c1b858ee0cc4d6d11f68c
Steve Cox <coxsteve@gmail.com>
2016-07-31 21:40:31 -0600

    corrected FrontPage layout, may need more work

--- Honest Badger/FrontPageViewController.swift
+++ Honest Badger/FrontPageViewController.swift
@@ -13,12 +13,18 @@ class FrontPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBarHidden = true
+        titleLabel.sizeToFit()
+        titleLabel.adjustsFontSizeToFitWidth = true
+        titleImageView.sizeToFit()
+        subTitleLabel.sizeToFit()
+        subTitleLabel.adjustsFontSizeToFitWidth = true
+        enterButton.sizeToFit()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleImageView: UIView!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var enterButton: UIButton!
     
     @IBAction func didPressEnterAnonymously(sender: AnyObject) {
         FIRAuth.auth()?.signInAnonymouslyWithCompletion() { (user, error) in
